fbb249982543b4e03fcd59d8769796f69f7abe36
Guliev Vakhid <01.unbroken@gmail.com>
2021-09-13 10:52:33 +0300

    Create entites

---
+++ Sql/Column.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sql
+{
+   public class Column
+    {
+        public int Id { get; }
+        public int RowId { get; }
+        public object DataType { get; }
+        public bool AllowNull { get; }
+    }
+}
---
+++ Sql/Row.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sql
+{
+   public  class Row
+    {
+        public int Id { get; }
+        public object Value { get; }
+    }
+}
